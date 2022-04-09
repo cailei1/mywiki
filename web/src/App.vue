e5277b94639864101aa41411c8262aac76688918
cailei <cailei@adesk.com>
2022-04-09 21:17:41 +0800

    添加前端工程，使用 Ant Design 初步完成首页布局搭建

---
+++ web/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+    <a-layout>
+        <a-layout-header class="header">
+            <div class="logo"/>
+            <a-menu
+                    v-model:selectedKeys="selectedKeys1"
+                    theme="dark"
+                    mode="horizontal"
+                    :style="{ lineHeight: '64px' }"
+            >
+                <a-menu-item key="1">nav 1</a-menu-item>
+                <a-menu-item key="2">nav 2</a-menu-item>
+                <a-menu-item key="3">nav 3</a-menu-item>
+            </a-menu>
+        </a-layout-header>
+        <router-view/>
+    </a-layout>
+    <a-layout-footer style="text-align: center">
+        OnePiece Wiki ©2022 Created by cailei
+    </a-layout-footer>
+</template>
+
+<style>
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+    }
+
+    #components-layout-demo-top-side-2 .logo {
+        float: left;
+        width: 120px;
+        height: 31px;
+        margin: 16px 24px 16px 0;
+        background: rgba(255, 255, 255, 0.3);
+    }
+
+    .ant-row-rtl #components-layout-demo-top-side-2 .logo {
+        float: right;
+        margin: 16px 0 16px 24px;
+    }
+
+    .site-layout-background {
+        background: #fff;
+    }
+</style>
