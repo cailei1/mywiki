34ebe6d37d1b36e83658090ed23c81321d77c56a
cailei <cailei@adesk.com>
2022-04-09 11:35:18 +0800

    1.添加Mybatic 自动生成代码 插件

--- doc/all.sql
+++ doc/all.sql
@@ -1,7 +1,18 @@
 drop table if exists `test`;
-create table `test`(
- `id` bigint not null comment 'id',
- `name` varchar(50) comment '名称',
- `password` varchar(250) comment '密码',
- primary key (`id`)
-)engine = innodb default charset = utf8mb4;
+create table `test`
+(
+    `id`       bigint not null comment 'id',
+    `name`     varchar(50) comment '名称',
+    `password` varchar(250) comment '密码',
+    primary key (`id`)
+) engine = innodb
+  default charset = utf8mb4;
+
+drop table if exists `demo`;
+create table `demo`
+(
+    `id`   bigint not null comment 'id',
+    `name` varchar(50) comment '名称',
+    primary key (`id`)
+) engine = innodb
+  default charset = utf8mb4;
