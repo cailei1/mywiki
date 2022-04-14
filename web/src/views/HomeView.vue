e881ea7013fff1e76805fefc658aed0762d4a68a
cailei <cailei@adesk.com>
2022-04-14 22:14:56 +0800

    前端页面显示

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -48,7 +48,29 @@
         <a-layout-content
                 :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '500px' }"
         >
-            <pre>{{eBooks}}</pre>
+
+            <a-list item-layout="vertical" size="large" :grid="{gutter: 20, column: 3}"
+                    :data-source="eBooks">
+                <template #renderItem="{ item }">
+                    <a-list-item key="item.title">
+                        <template #actions>
+          <span v-for="{ type, text } in actions" :key="type">
+            <component :is="type" style="margin-right: 8px"/>
+            {{ text }}
+          </span>
+                        </template>
+                        <a-list-item-meta :description="item.description">
+                            <template #title>
+                                <a :href="item.href">{{ item.name }}</a>
+                            </template>
+                            <template #avatar>
+                                <a-avatar :src="item.cover"/>
+                            </template>
+                        </a-list-item-meta>
+                        {{ item.content }}
+                    </a-list-item>
+                </template>
+            </a-list>
 
         </a-layout-content>
     </a-layout>
@@ -62,18 +84,49 @@
 <!--</script>-->
 
 <script>
+    import {StarOutlined, LikeOutlined, MessageOutlined} from '@ant-design/icons-vue';
     import {defineComponent, onMounted, ref} from 'vue';
     import HelloWorld from '@/components/HelloWorld.vue';
     import axios from 'axios';
+
+    const listData = [];
+
+    for (let i = 0; i < 23; i++) {
+        listData.push({
+            href: 'https://www.antdv.com/',
+            title: `ant design vue part ${i}`,
+            avatar: 'https://joeschmoe.io/api/v1/random',
+            description:
+                'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+            content:
+                'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+        });
+    }
 
 
     export default defineComponent({
         name: 'HomeView',
         components: {
             HelloWorld,
+            StarOutlined,
+            LikeOutlined,
+            MessageOutlined,
         },
         setup() {
             const eBooks = ref();
+
+            const pagination = {
+                onChange: (page) => {
+                    console.log(page);
+                },
+                pageSize: 3,
+            };
+
+            const actions = [
+                {type: 'StarOutlined', text: '156'},
+                {type: 'LikeOutlined', text: '156'},
+                {type: 'MessageOutlined', text: '2'},
+            ];
 
             onMounted(() => {
                 axios.get("http://localhost:8888/books").then(response => {
@@ -84,8 +137,37 @@
                 });
             });
 
-            return {eBooks}
+            return {
+                eBooks,
+                listData,
+                pagination,
+                actions,
+            }
         }
 
     });
 </script>
+
+<style scoped>
+    .ant-avatar {
+        margin: 5px 0;
+        padding: 0;
+        color: rgba(0, 0, 0, 0.85);
+        font-size: 14px;
+        font-variant: tabular-nums;
+        line-height: 50px;
+        list-style: none;
+        font-feature-settings: 'tnum';
+        position: relative;
+        display: inline-block;
+        overflow: hidden;
+        color: #fff;
+        white-space: nowrap;
+        text-align: center;
+        vertical-align: middle;
+        background: #ccc;
+        width: 50px;
+        height: 50px;
+        border-radius: 8%;
+    }
+</style>
