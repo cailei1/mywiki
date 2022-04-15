0fbf74090fccb52c6f1ea101854daea25e7d6a25
cailei <cailei@adesk.com>
2022-04-15 15:23:03 +0800

    修改电子书管理界面

--- web/src/views/admin/EbookManagerPage.vue
+++ web/src/views/admin/EbookManagerPage.vue
@@ -1,5 +1,12 @@
 <template>
-    <div class="EbookManager"></div>
+    <a-layout>
+        <a-layout-content
+                :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '500px' }"
+        >
+           电子书管理界面
+
+        </a-layout-content>
+    </a-layout>
 </template>
 
 
