bc043f318d40a2638784ecc3b6427e625fe6b17a
cailei <cailei@adesk.com>
2022-04-15 14:55:22 +0800

    增加电子书管理界面，路由跳转功能

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,25 +1,32 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import {createRouter, createWebHistory, RouteRecordRaw} from 'vue-router'
+import HomeView from '../views/HomeView.vue';
+import AboutView from '../views/AboutView.vue';
+import EbookManagerPage from '../views/admin/EbookManagerPage.vue';
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    {
+        path: '/',
+        name: 'home',
+        component: HomeView
+    },
+    {
+        path: '/about',
+        name: 'about',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: AboutView
+    },
+    {
+        path: '/ebook_manager',
+        name: 'ebookManager',
+        component: EbookManagerPage
+    }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 export default router
---
+++ web/src/views/admin/EbookManagerPage.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="EbookManager"></div>
+</template>
+
+
