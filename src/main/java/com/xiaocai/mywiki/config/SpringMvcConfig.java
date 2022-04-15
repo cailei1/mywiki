b251984435eff72af61ccd6b425314347f59c2ea
cailei <cailei@adesk.com>
2022-04-15 11:24:37 +0800

    增加拦截器，打印接口耗时

---
+++ src/main/java/com/xiaocai/mywiki/config/SpringMvcConfig.java
@@ -0,0 +1,35 @@
+package com.xiaocai.mywiki.config;
+
+
+import com.xiaocai.mywiki.interceptor.LogInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    LogInterceptor loginInterceptor;
+
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/test/**",
+                        "/redis/**",
+                        "/user/login",
+                        "/category/all",
+                        "/ebook/list",
+                        "/doc/all/**",
+                        "/doc/vote/**",
+                        "/doc/find-content/**",
+                        "/ebook-snapshot/**"
+                );
+
+    }
+}
+
