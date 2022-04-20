c7ed50317216ae8d540b0fe490708f916eae4036
cailei <cailei@adesk.com>
2022-04-20 17:51:16 +0800

    分类表设计与持久层代码生成

--- doc/all.sql
+++ doc/all.sql
@@ -22,6 +22,32 @@ insert into `ebook` (id, name, description) values (4, 'Mysql 入门教程', '�
 insert into `ebook` (id, name, description) values (5, 'Oracle 入门教程', '零基础入门 Oracle 开发，企业级应用开发最佳首选框架');
 
 
+# 分类表
+drop table if exists `category`;
+create table `category`(
+    `id` bigint not null comment 'id',
+    `parent` bigint not null default 0 comment '父id',
+    `name` varchar(50) not null comment '名称',
+    `sort` int comment '顺序',
+    primary key (`id`)
+)engine = innodb default charset = utf8mb4 comment = '分类';
+
+insert into `category` (id,parent,name,sort) values (100,000,'前端开发',100);
+insert into `category` (id,parent,name,sort) values (101,100,'Vue',101);
+insert into `category` (id,parent,name,sort) values (102,100,'HTML & CSS',102);
+insert into `category` (id,parent,name,sort) values (200,000,'JAVA',200);
+insert into `category` (id,parent,name,sort) values (201,200,'基础应用',201);
+insert into `category` (id,parent,name,sort) values (202,200,'框架应用',202);
+insert into `category` (id,parent,name,sort) values (300,000,'Python',300);
+insert into `category` (id,parent,name,sort) values (301,300,'基础应用',301);
+insert into `category` (id,parent,name,sort) values (302,300,'进阶方向应用',302);
+insert into `category` (id,parent,name,sort) values (400,000,'数据库',400);
+insert into `category` (id,parent,name,sort) values (401,400,'Mysql',401);
+insert into `category` (id,parent,name,sort) values (500,000,'其他',500);
+insert into `category` (id,parent,name,sort) values (501,500,'服务器',501);
+insert into `category` (id,parent,name,sort) values (502,500,'热门语言',502);
+insert into `category` (id,parent,name,sort) values (503,500,'开发工具',503);
+
 drop table if exists `test`;
 create table `test`
 (
@@ -40,3 +66,4 @@ create table `demo`
     primary key (`id`)
 ) engine = innodb
   default charset = utf8mb4;
+
